3ee0036027f8cd04d63332d2730a5a7782aad263
AdaMo22805 <165852075+AdaMo22805@users.noreply.github.com>
2025-01-22 16:17:05 -0800

    automated navigation menu in js

---
+++ global.js
@@ -0,0 +1,34 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+    { url: 'index.html', title: 'Home' },
+    { url: 'projects/index.html', title: 'Projects' },
+    { url: 'contact/index.html', title: 'Contact' },
+    { url: 'resume.html', title: 'Resume' },
+    { url: 'https://github.com/AdaMo22805.html', title: 'Github' },
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host != location.host) {
+        a.target = "_blank";
+    }
+  }
