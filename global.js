d1e89e68581cf0d9a05078e460ddabd9d9455d8f
AdaMo22805 <165852075+AdaMo22805@users.noreply.github.com>
2025-01-23 22:25:21 -0800

    color scheme & working contact page

--- global.js
+++ global.js
@@ -32,3 +32,46 @@ for (let p of pages) {
         a.target = "_blank";
     }
   }
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select class=one>
+            <option value="light dark"> Automatic </option>
+            <option value="light"> Light </option>
+            <option value="dark"> Dark </option>
+          </select>
+      </label>`
+  );
+
+let select = document.querySelector(".one");
+
+if ("colorScheme" in localStorage) {
+  const savedScheme = localStorage.colorScheme;
+  document.documentElement.style.setProperty('color-scheme', savedScheme);
+  select.value = savedScheme;
+}
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value
+  });
+
+let form = document.querySelector("form");
+form?.addEventListener('submit', function (event) {
+  // console.log('pressed submit');
+  event.preventDefault();
+  let data = new FormData(form);
+  let url = form.action + "?";
+  for (let [name, value] of data) {
+    // TODO build URL parameters here
+    value = encodeURIComponent(value)
+    console.log(name, value);
+    url += `${encodeURIComponent(name)}=${value}&`;
+  }
+  console.log("Final URL:", url);
+  location.href = url;
+});
