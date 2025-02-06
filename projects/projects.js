b48d1874bf1cb3a89ac63c17f662a5617606377f
AdaMo22805 <165852075+AdaMo22805@users.noreply.github.com>
2025-02-06 14:42:29 -0800

    search bar formatted and functional

--- projects/projects.js
+++ projects/projects.js
@@ -91,3 +91,17 @@ data.forEach((d, idx) => {
 // let data = rolledData.map(([year, count]) => {
 //     return { value: count, label: year };
 //   });
+
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('change', (event) => {
+  // update query value
+  query = event.target.value;
+  // filter projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  // render filtered projects
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+});
