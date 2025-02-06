669e6037ec7a319178d556a27e61fd37a4a7d445
AdaMo22805 <165852075+AdaMo22805@users.noreply.github.com>
2025-02-05 23:21:25 -0800

    create pie chart legend

--- projects/projects.js
+++ projects/projects.js
@@ -22,7 +22,16 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 
 
-let data = [1, 2, 3, 4, 5, 5];
+// let data = [1, 2, 3, 4, 5, 5];
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
+
 // let colors = ['gold', 'purple'];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
@@ -40,7 +49,7 @@ let colors = d3.scaleOrdinal(d3.schemeTableau10);
 //   arcData.push({ startAngle: angle, endAngle });
 //   angle = endAngle;
 // }
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -50,5 +59,10 @@ arcs.forEach((arc,idx) => {
     d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx));
 });
 
-
-
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+        .attr('class', 'swatch')
+        .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+})
